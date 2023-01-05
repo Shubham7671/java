94d0e3459d1bda5002a8fcd04f45f89546299585
Shubham7671 <sk767196@gmail.com>
2023-01-06 00:20:24 +0530

    learning

---
+++ src/basics/Learning/array01.java
@@ -0,0 +1,26 @@
+package src.basics.Learning;
+
+import java.util.Scanner;
+
+public class array01 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        int product=1;
+        for(int i=n-1;i>=0;i--){
+            if(i%2==0){
+                sum+=arr[i];
+            }
+            if(i%2!=0){
+                product=product*arr[i];
+            }
+        }
+        System.out.println(sum);
+        System.out.println(product);
+    }
+}
