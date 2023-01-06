2b612d7e79a77ac6f2473c2ed084cb154360b8b1
Shubham7671 <sk767196@gmail.com>
2023-01-06 21:09:39 +0530

    learning

--- src/basics/Learning/pattern/pattern05.java
+++ src/basics/Learning/pattern/pattern05.java
@@ -1,7 +1,26 @@
 package src.basics.Learning.pattern;
+import java.util.Scanner;
 
+
+//5
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        * * * * *
 public class pattern05 {
     public static void main(String[] args) {
 
+      Scanner sc= new Scanner(System.in);
+      int num=sc.nextInt();
+      for(int i=1;i<=num;i++){
+          for(int k=1;k<=num-i;k++){
+              System.out.print("  ");
+          }
+          for(int j=1;j<=i;j++){
+          System.out.print("* ");
+          }
+          System.out.println();
+      }
     }
 }
