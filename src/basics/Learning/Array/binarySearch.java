efe95be8bc7097b66c8e83210ff9f7f683da77ee
Shubham7671 <sk767196@gmail.com>
2023-01-13 22:07:44 +0530

    learning

---
+++ src/basics/Learning/Array/binarySearch.java
@@ -0,0 +1,33 @@
+package src.basics.Learning.Array;
+import  java.util.*;
+
+import java.lang.reflect.Array;
+
+public class binarySearch {
+    static  int binaryS(int []arr,int key){
+        Arrays.sort(arr);
+        int low=0;
+        int high=arr.length-1;
+
+
+        for (int i = low; i <= high; i++) {
+            int mid=(high+low)/2;
+
+                if(arr[mid]==key){
+                    return mid;
+                }
+            else if(arr[mid]<key){
+                 low=mid+1;
+                }
+            else if (arr[mid]>key){
+                high=mid -1;
+            }
+        }
+ return -1;
+    }
+    public static void main(String[] args) {
+        int []arr={1,2,3,4,5};
+        int key=5;
+        System.out.println( binaryS(arr,key));
+    }
+}
