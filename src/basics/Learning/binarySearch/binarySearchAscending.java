24e75c264640a9db6ec5e6b1ac7e7326b9df5a39
Shubham7671 <sk767196@gmail.com>
2023-01-15 14:28:33 +0530

    --

---
+++ src/basics/Learning/binarySearch/binarySearchAscending.java
@@ -0,0 +1,29 @@
+package src.basics.Learning.binarySearch;
+
+public class binarySearchAscending {
+    static int binaryAsen(int[]arr,int k){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            else if (arr[mid]<k){
+            start=mid+1;
+                }
+            else if (arr[mid]>k) {
+                end=mid-1;
+            }
+        }
+        return -1;
+        }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9,10,11,14,75,89,};
+
+        int ans=binaryAsen(arr,5);
+        System.out.println(ans);
+
+    }
+}
---
+++ src/basics/Learning/binarySearch/binarySearchDescending.java
@@ -0,0 +1,27 @@
+package src.basics.Learning.binarySearch;
+
+public class binarySearchDescending {
+    static int binarySearchDes(int[]arr,int k){
+        int start=0;
+        int end= arr.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+
+            if(arr[mid]==k){
+                return mid;
+            } else if (arr[mid]<k) {
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]={90,88,52,42,36,24,21,12,8,7,6,2,1,0};
+
+        int ans=binarySearchDes(arr,0);
+        System.out.println(ans);
+    }
+}
