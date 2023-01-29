eaf626093eedb1ce09dd6d4b508e15ab81829202
Shubham7671 <sk767196@gmail.com>
2023-01-29 21:43:05 +0530

    --

---
+++ src/basics/Learning/pattern/pattern12.java
@@ -0,0 +1,30 @@
+package src.basics.Learning.pattern;
+
+import java.util.Scanner;
+
+public class pattern12 {
+    public static void main(String[] args)
+    {
+        // Create a new Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the number of rows from the user
+        System.out.println("Enter the number of rows needed in the pattern ");
+
+        int rows = scanner.nextInt();
+
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = rows; j > i; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                System.out.print("*"+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
