10c275e3d7d7322874934bcb82f0ce0f84016a7d
Shubham7671 <sk767196@gmail.com>
2023-02-05 18:27:59 +0530

    ---

---
+++ src/basics/Learning/examples/McqExam.java
@@ -0,0 +1,15 @@
+package src.basics.Learning.examples;
+import java.util.*;
+public class McqExam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int P = sc.nextInt();
+        int Q = sc.nextInt();
+        int R = sc.nextInt();
+        int S = sc.nextInt();
+        int T = (P + Q + R + S);
+        int A  = Math.max(Math.max(Math.max(P,Q),R),S);
+        if (T==100);
+        System.out.println(A);
+    }
+}
