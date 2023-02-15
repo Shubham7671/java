86e660980a41fbcbc163844b7bd081842c5c2e78
Shubham7671 <sk767196@gmail.com>
2023-02-16 00:23:23 +0530

    learning

--- src/basics/Learning/Recursion/factorialOfNumber.java
+++ src/basics/Learning/Recursion/factorialOfNumber.java
@@ -1,17 +1,17 @@
 package src.basics.Learning.Recursion;
 
 public class factorialOfNumber {
-    static int calcFactorail(int n){
+    static int calcFactorial(int n){
         if(n==0||n==1){
             return 1;
         }
-        int fact=calcFactorail(n-1);
+        int fact=calcFactorial(n-1);
         int ans=n*fact;
         return ans;
     }
     public static void main(String[] args) {
         int n=5;
-        int ans=calcFactorail(n);
+        int ans=calcFactorial(n);
         System.out.println(ans);
     }
 }
