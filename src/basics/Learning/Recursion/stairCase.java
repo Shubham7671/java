c3ae4fc379b45437f6128c0cc4485124416e12dd
Shubham7671 <sk767196@gmail.com>
2023-02-19 14:50:44 +0530

    learning

--- src/basics/Learning/Recursion/stairCase.java
+++ src/basics/Learning/Recursion/stairCase.java
@@ -6,7 +6,6 @@ public class stairCase {
         if (n <= 1) return 1;
 
         return (climbStairs(n - 1) + climbStairs(n - 2));
-
     }
     public static void main(String[] args) {
        int n=2;
