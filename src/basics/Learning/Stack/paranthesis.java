27476756df7c964b650a6cb065c7bba17caaea51
Shubham7671 <sk767196@gmail.com>
2023-03-04 22:27:43 +0530

    learning

---
+++ src/basics/Learning/Stack/paranthesis.java
@@ -0,0 +1,39 @@
+package src.basics.Learning.Stack;
+
+import java.util.Stack;
+
+public class paranthesis {
+    public static void main(String[] args) {
+        String s="(((()))";
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+
+            if(s.charAt(i)=='{'||s.charAt(i)=='('||s.charAt(i)=='['){
+
+                st.push(s.charAt(i));
+
+            } else if (s.charAt(i) == ')' && !st.isEmpty() && st.peek() == '('){
+
+                st.pop();
+
+            }else if (s.charAt(i)=='}'&&!st.isEmpty()&&st.peek()=='{'){
+
+                st.pop();
+
+            }else if (s.charAt(i)==']'&&!st.isEmpty()&&st.peek()=='['){
+
+                st.pop();
+
+            }else {
+
+                System.out.print("False");
+
+                return;
+
+            }
+
+        }
+
+        System.out.print(st.isEmpty());
+    }
+}
