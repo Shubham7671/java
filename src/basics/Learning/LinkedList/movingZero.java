5b7c8f1c0650a28719f301b5303bef108251ae58
Shubham7671 <sk767196@gmail.com>
2023-03-18 15:15:37 +0530

    learning

---
+++ src/basics/Learning/LinkedList/movingZero.java
@@ -0,0 +1,17 @@
+package src.basics.Learning.LinkedList;
+
+import java.util.LinkedList;
+
+public class movingZero {
+    public static void main(String[] args) {
+        LinkedList<Integer>ln=new LinkedList<>();
+        ln.add(0);
+        ln.add(1);
+        ln.add(0);
+        ln.add(1);
+        ln.add(0);
+        ln.add(2);
+
+    }
+
+}
---
+++ src/basics/Learning/Stack/reversalofstack.java
@@ -0,0 +1,22 @@
+package src.basics.Learning.Stack;
+
+import java.util.Stack;
+
+public class reversalofstack {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        Stack<Integer> st=new Stack<>();
+        Stack<Integer> ansSt=new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            st.push(arr[i]);
+        } //1  2 3 4 5
+
+        for (int i = 0; i < arr.length; i++) {
+            ansSt.push(st.pop());
+        }
+        while(!ansSt.isEmpty()){
+            System.out.println(ansSt.pop());
+        }
+
+    }
+}
