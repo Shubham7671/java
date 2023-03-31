a6db64b4f6fe764e7ff2a4da49e11e8d5a4ec639
Shubham7671 <sk767196@gmail.com>
2023-03-31 23:29:16 +0530

    learning

--- src/basics/Learning/pattern/pattern01.java
+++ src/basics/Learning/pattern/pattern01.java
@@ -11,6 +11,8 @@ import java.util.StringJoiner;
         * * * * * * *
         * * * * * * *    */
 
+
+
 public class pattern01 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
